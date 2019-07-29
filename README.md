f9ea9f0ce319391b749af1d413a482b8f1caf77e
shinedark <mide0303@gmail.com>
2019-07-29 17:47:58 -0500

    html page

--- README.md
+++ README.md
@@ -2,8 +2,10 @@ node-web-scraper
 repo from tutorial https://github.com/scotch-io/node-web-scraper 
 ``` shell
 $ npm install
-$ node server.js
+$ nodemon 
 ```
+
+got to http://localhost:8081/ to see the list to scrape go to http://localhost:8081/scrape
 
 You can scrape Yahoo finance table for the 100 companies listed their 1st table page.
 It also includes other links that are in tables in the site but if you check [output](./output.json)
--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express');
+var router = express.Router();
 var fs      = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
+var list  = require('./output.json')
 var app     = express();
+
+
+app.use(express.static('public'));
+
+app.get('/output.json', function(req, res, next) {
+  res.sendFile(__dirname + '/output.json');
+})
 
 app.get('/scrape', function(req, res){
   url = 'https://finance.yahoo.com/screener/predefined/ms_real_estate?offset=0&count=100';
@@ -12,7 +21,7 @@ app.get('/scrape', function(req, res){
     links = $('a'); //jquery get all hyperlinks
     $(links).each(function(i, link){
      
-      json.info.push(i, $(link).text() + ':\n  ' +'https://finance.yahoo.com' + $(link).attr('href'))
+      json.info.push({name: $(link).text(),link: 'https://finance.yahoo.com' +  $(link).attr('href')})
     });
     fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
       console.log('File successfully written! - Check your project directory for the output.json file');
